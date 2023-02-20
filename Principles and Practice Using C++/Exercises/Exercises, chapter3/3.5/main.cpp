eebe1dc156fd94e53392eb36cd370e31174c4c41
Ivan <124918375+steinerivan@users.noreply.github.com>
2023-02-20 11:04:48 +0100

    Create main.cpp

---
+++ Principles and Practice Using C++/Exercises/Exercises, chapter3/3.5/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Упражнения, глава 3.5
+//
+//  Created by Ivan Voloshyn on 22.01.23.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Введите два значения через пробел!\n";
+    double val1, val2;
+    cin >> val1 >> val2;
+    if(val1>val2){
+        cout << val1 << " - наибольшее значение\n";
+    }
+    else if(val1<val2){
+        cout << val2 << " - наибольшее значение\n";
+    }
+    else{
+        cout << "Оба значения одинаковы";
+    }
+    cout << "Cумма - " << val1 + val2 << ".\n Разность - " << val1 - val2 << ".\n Произведение - " << val1*val2 << ".\n Частное - " << val1/val2 << ".\n";
+    return 0;
+}
