b48e20fb89b1a46e7e3ce86e12a1b26bdc8b7273
Ivan <124918375+steinerivan@users.noreply.github.com>
2023-02-20 11:02:54 +0100

    Create main.cpp

---
+++ Principles and Practice Using C++/Exercises/Exercises, chapter3/3.3/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Упражнение, глава 3.3
+//
+//  Created by Ivan Voloshyn on 22.01.23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 5.5;
+    char j = 'j';
+    string words = "words";
+    int double = 5;
+    char for = 78;
+    string Main_Words = 150;
+    return 0;
+}
