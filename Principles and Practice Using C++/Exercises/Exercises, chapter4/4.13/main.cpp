447f83b121003fecc2fda35117c2a622b0ffaab5
Ivan <124918375+steinerivan@users.noreply.github.com>
2023-02-20 11:37:30 +0100

    Create main.cpp

---
+++ Principles and Practice Using C++/Exercises/Exercises, chapter4/4.13/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Упражнения, глава 4.13
+//
+//  Created by Ivan Voloshyn on 23.01.23.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    vector<int> v, newv;
+    int i, p = 2, x, two = 2;
+    for(i = 2; i <= 100; i++){
+        v.push_back(i);
+    }
+    for(x=p*two; x <=100; two++){
+        if()
+        }
+    return 0;
+}
