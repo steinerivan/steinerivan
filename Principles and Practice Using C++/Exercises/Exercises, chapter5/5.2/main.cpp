eec5243aee0ed72384375b43ed33fe976e592380
Ivan <124918375+steinerivan@users.noreply.github.com>
2023-02-23 19:40:10 +0100

    Create main.cpp

---
+++ Principles and Practice Using C++/Exercises/Exercises, chapter5/5.2/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Ivan Voloshyn on 23.02.23.
+//
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+double ctok(double c){
+    double k = c + 273.15;
+    return k;
+}
+
+int main() {
+    double c = 0;
+    cin >> c;
+    double k = ctok(c);
+    cout << k << "\n";
+    return 0;
+}
