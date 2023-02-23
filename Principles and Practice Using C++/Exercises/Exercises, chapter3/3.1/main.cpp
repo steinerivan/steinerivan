4c879547487760f9da551b0e64e24c9f54c7ce5a
Ivan <124918375+steinerivan@users.noreply.github.com>
2023-02-23 19:06:03 +0100

    Create main.cpp

---
+++ Principles and Practice Using C++/Exercises/Exercises, chapter3/3.1/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  exercise3.1
+//
+//  Created by Ivan Voloshyn on 23.02.23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter your name!\n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hello, " << first_name << "\n";
+    
+    return 0;
+}
